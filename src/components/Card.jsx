633588b15c99aa2e23c30a467a1319e6afda4f74
jazz <jazxbx@gmail.com>
2023-08-27 14:05:01 -0700

    added isAlive props to switch card view

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,23 @@
-const Card = ({ name, emoji, hunger, love, feedPet, lovePet }) => (
-  <div className="card">
-    <div className="emoji" onClick={lovePet}>
-      {emoji}
+const Card = ({ name, emoji, hunger, love, feedPet, lovePet, isAlive }) => {
+  return (
+    <div className="card">
+      {isAlive ? (
+        <>
+          <div className="emoji" onClick={lovePet}>
+            {emoji}
+          </div>
+          <p>{name}</p>
+          <p>Hunger:</p>
+          <meter max="100" min="0" value={hunger}></meter>
+          <p>Love:</p>
+          <meter max="100" min="0" value={love}></meter>
+          <button onClick={feedPet}>Feed Me 🍞</button>
+        </>
+      ) : (
+        <p>Sorry ma brutha</p>
+      )}
     </div>
-    <p>{name}</p>
-    <p>Hunger:</p>
-    <meter max="100" min="0" value={hunger}></meter>
-    <p>Love:</p>
-    <meter max="100" min="0" value={love}></meter>
-    <button
-      onClick={feedPet}
-      // was not working due to forgetting the parenthesis to call the onFeedClick()
-
-      //   onClick={() => {
-      //     onFeedClick;
-      //   }}
-    >
-      Feed Me 🍞
-    </button>
-  </div>
-);
+  );
+};
 
 export { Card };
